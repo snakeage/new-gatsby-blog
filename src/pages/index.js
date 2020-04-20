892be49db2d0bd917bc6b7c414adf2ff6e3667a3
Alexander <Snakeage@gmail.com>
2020-04-20 12:21:49 +0300

    adding all our new blog code for gatsby setup

---
+++ src/pages/index.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: node;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+export default ({ data }) =>{ 
+  console.log(data)
+  return (
+  <Layout>
+    <SEO title="Home" />
+    <div>
+    <h1>Alexander's Thoughts</h1>
+    <h4>{ data.allMarkdownRemark.totalCount }</h4>
+    {
+      data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>{ node.frontmatter.title } - {node.frontmatter.date}</BlogTitle>
+          </BlogLink>
+          <p>{node.excerpt}</p>
+        </div>
+      
+      ))
+    }
+    </div>
+  </Layout>
+)}
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
+            date
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  } 
+`
---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLLO FROM THE THIRD PAGE</h1>
+  </Layout>
+)
+
+export default ThirdPage
